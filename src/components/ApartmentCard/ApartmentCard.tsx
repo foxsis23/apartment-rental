5536cb9774f5b2da17ba52407e36c87479c1629a
Illia Strilets <56593640+foxsis23@users.noreply.github.com>
2025-11-09 18:36:08 +0200

    card fix

--- src/components/ApartmentCard/ApartmentCard.tsx
+++ src/components/ApartmentCard/ApartmentCard.tsx
@@ -12,35 +12,28 @@ export const ApartmentCard = ({ apartment }: ApartmentCardProps) => {
     <div className={styles.card}>
       <Carousel className="w-full max-w-xs">
         <CarouselContent>
-          {apartment.photos.map((photo, index) => (
+          {apartment.photosUrls.map((photo, index) => (
             <CarouselItem key={index}>
               <img src={photo} alt="apartment-picture" className={styles.img} />
             </CarouselItem>
           ))}
         </CarouselContent>
       </Carousel>
-      <div className={styles.cardInfo}>
-        <h3 className="font-bold text-emerald-500 text-2xl">$ {apartment.pricePerMonth}</h3>
-        <span className="text-sm text-gray-500">
-          {apartment.address}, {apartment.district}
-        </span>
-        <p className="text-sm line-clamp-2">{apartment.description}</p>
-        <div className={styles.cardDetails}>
-          <span className="p-2 bg-gray-400 text-white rounded-md">
-            {apartment.roomsCount} кімнат
+      <div className="flex flex-col justify-between">
+        <div className={styles.cardInfo}>
+          <h3 className="font-bold text-emerald-500 text-2xl">$ {apartment.price}</h3>
+          <span className="text-sm text-gray-500">
+            {apartment.street}, {apartment.district}
           </span>
-          <span className="p-2 bg-gray-400 text-white rounded-md">
-            меблі {apartment.furnished ? '+' : '-'}
-          </span>
-          <span className="p-2 bg-gray-400 text-white rounded-md">
-            pet Friendly {apartment.petsAllowed ? '+' : '-'}
-          </span>
-          <span className="p-2 bg-gray-400 text-white rounded-md">
-            {apartment.floor} поверх з {apartment.totalFloors}
-          </span>
-          <span className="p-2 bg-gray-400 text-white rounded-md">площа {apartment.area} м²</span>
+          <p className="text-sm line-clamp-2">{apartment.description}</p>
+          <div className={styles.cardDetails}>
+            <span className={styles.cardDetail}>{apartment.roomsCount} кімнат</span>
+            <span className={styles.cardDetail}>меблі {apartment.furnitureType ? '+' : '-'}</span>
+            <span className={styles.cardDetail}>{apartment.floor} поверх</span>
+            <span className={styles.cardDetail}>площа {apartment.area} м²</span>
+          </div>
         </div>
-        <Button variant="link" className="self-start mt-3">
+        <Button variant="link" className="self-start mt-3 cursor-pointer">
           Детальніше
         </Button>
       </div>
